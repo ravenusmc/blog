351af505137b84de435f58a6cbfdb737ed61b49d
ravenusmc <mcuddy77@gmail.com>
2019-02-16 10:56:59 -0500

    finished sign up form on sign up page

--- signup.php
+++ signup.php
@@ -1,39 +1,28 @@
 <?php
-  // Start the session
-  session_start();
+
+  // Start a session
+  if (!isset($_SESSION)) {
+    session_start();
+  }
   //Pulling in the databases
   require('./model/database.php');
   global $db;
   $message = "";
   if (isset($_POST["login"])){
+    $firstname = filter_input(INPUT_POST, 'firstname');
+    $lastname = filter_input(INPUT_POST, 'lastname');
     $username = filter_input(INPUT_POST, 'username');
+    $email = filter_input(INPUT_POST, 'email');
+    $city = filter_input(INPUT_POST, 'city');
+    $state = filter_input(INPUT_POST, 'state');
+    $zip = filter_input(INPUT_POST, 'zip');
     $password = filter_input(INPUT_POST, 'password');
     $password2 = filter_input(INPUT_POST, 'password2');
+
     //Hashing the password
     $password_hashed = password_hash($password, PASSWORD_DEFAULT);
-    $query = "SELECT * FROM users WHERE
-              userName = :username AND
-              password = :password";
-    $statement = $db->prepare($query);
-    $statement->bindValue(':username', $username);
-    $statement->bindValue(':password', $password);
-    $statement->execute();
-    $count = $statement->rowCount();
-    if ($count > 0){
-      $message = '<label>Username Taken!</label>';
-    }else if ($password != $password2) {
-      $message = '<label class="errorMsg">Passwords Do Not Match!</label>';
-    }else {
-      $query = 'INSERT INTO users
-                  (userName, password)
-                VALUES
-                  (:username, :password)';
-      $statement = $db->prepare($query);
-      $statement->bindValue(':username', $username);
-      $statement->bindValue(':password', $password_hashed);
-      $statement->execute();
-      $statement->closeCursor();
-    }
+
+
   }
 ?>
 <?php include 'view/header.php'; ?>
@@ -43,56 +32,64 @@
 <link rel="stylesheet" type="text/css" href="./assets/css/signup.css"> -->
 
 <div class="row">
-  <form class="col s12">
+  <!-- Start of error handling -->
+  <?php
+    if (isset($message)){
+      echo $message;
+    }
+  ?>
+  <!-- End of error handling -->
+  <form method="post" class="col s12">
     <div class="row">
       <div class="input-field col s6">
-        <input id="first_name" type="text" class="validate">
+        <!-- <input name='firstname' id="first_name" type="text" class="validate"> -->
+        <input type="text" name='firstname' class="validate" id="firstname">
         <label for="first_name">First Name</label>
       </div>
       <div class="input-field col s6">
-        <input id="last_name" type="text" class="validate">
+        <input name='lastname' id="last_name" type="text" class="validate">
         <label for="last_name">Last Name</label>
       </div>
     </div>
     <div class="row">
       <div class="input-field col s12">
-        <input id="email" type="email" class="validate">
+        <input name='username' id="email" type="email" class="validate">
         <label for="email">Username</label>
       </div>
     </div>
     <div class="row">
       <div class="input-field col s12">
-        <input id="email" type="email" class="validate">
+        <input name='email' id="email" type="email" class="validate">
         <label for="email">Email</label>
       </div>
     </div>
     <div class="row">
       <div class="input-field col s6">
-        <input id="first_name" type="text" class="validate">
+        <input name='city' id="first_name" type="text" class="validate">
         <label for="first_name">City</label>
       </div>
       <div class="input-field col s6">
-        <input id="last_name" type="text" class="validate">
+        <input name='state' id="last_name" type="text" class="validate">
         <label for="last_name">State</label>
       </div>
       <div class="input-field col s6">
-        <input id="last_name" type="text" class="validate">
+        <input name='zip' id="last_name" type="text" class="validate">
         <label for="last_name">Zip</label>
       </div>
     </div>
     <div class="row">
       <div class="input-field col s12">
-        <input id="password" type="password" class="validate">
+        <input name='password' id="password" type="password" class="validate">
         <label for="password">Password</label>
       </div>
     </div>
     <div class="row">
       <div class="input-field col s12">
-        <input id="password" type="password" class="validate">
-        <label for="password">Confirm Password</label>
+        <input name='password2' id="password2" type="password" class="validate">
+        <label for="password2">Confirm Password</label>
       </div>
     </div>
-    <a class="waves-effect waves-light btn">Submit</a>
+    <button type="submit" name="login" class="btn btn-primary form-submit-btn">Sign Up</button>
   </form>
 </div>
 
