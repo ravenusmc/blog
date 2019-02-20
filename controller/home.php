cf4b007f3e17942d378e81bbb6627cc9cbf80d69
ravenusmc <mcuddy77@gmail.com>
2019-02-19 20:04:44 -0500

    working on home page

--- controller/home.php
+++ controller/home.php
@@ -11,8 +11,36 @@
 
 <?php include '../view/header.php'; ?>
 
+<!-- start of main section -->
 <main>
+
+  <div class="row">
+
+    <div class="col s3">
+      <div class="card-panel orange lighten-2">
+        <span class="white-text">Share you ideas with others...
+        </span>
+      </div>
+    </div>
+
+    <div class="col s3">
+      <div class="card-panel orange lighten-2">
+        <span class="white-text">Argue with others...
+        </span>
+      </div>
+    </div>
+
+    <div class="col s3">
+      <div class="card-panel orange lighten-2">
+        <span class="white-text">Be inspired by new ideas...
+        </span>
+      </div>
+    </div>
+
+  </div>
+
 </main>
+<!-- End of Main Section -->
 
 
 <?php include '../view/footer.php'; ?>
