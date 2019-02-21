b26a810185d264abd63dfcbcb269811cbea3ec66
ravenusmc <mcuddy77@gmail.com>
2019-02-20 19:56:32 -0500

    working on section two

--- controller/home.php
+++ controller/home.php
@@ -14,30 +14,46 @@
 <!-- start of main section -->
 <main>
 
-  <div class="row">
+  <section class='section_one'>
+    <div class="row">
 
-    <div class="col s3">
-      <div class="card-panel orange lighten-2">
-        <span class="white-text">Share you ideas with others...
-        </span>
+      <div class="col s3">
+        <div class="card-panel orange lighten-2">
+          <span class="white-text">Share you ideas with others...
+          </span>
+        </div>
       </div>
-    </div>
 
-    <div class="col s3">
-      <div class="card-panel orange lighten-2">
-        <span class="white-text">Argue with others...
-        </span>
+      <div class="col s3">
+        <div class="card-panel orange lighten-2">
+          <span class="white-text">Argue with others...
+          </span>
+        </div>
       </div>
-    </div>
 
-    <div class="col s3">
-      <div class="card-panel orange lighten-2">
-        <span class="white-text">Be inspired by new ideas...
-        </span>
+      <div class="col s3">
+        <div class="card-panel orange lighten-2">
+          <span class="white-text">Be inspired by new ideas...
+          </span>
+        </div>
       </div>
-    </div>
 
-  </div>
+    </div>
+  </section>
+
+  <section class='section_two'>
+
+    <form action="index.php" method="post">
+      <label>Please Select a Topic:</label>
+      <select name="officers">
+        <?php foreach ($officers as $officer) : ?>
+        <option value='<?php echo $officer['officer_id'] ?>'>
+          <?php echo $officer['first']; ?> <?php echo $officer['last']; ?></option>
+        <?php endforeach; ?>
+      </select><br>
+    </form>
+
+  </section>
 
 </main>
 <!-- End of Main Section -->
